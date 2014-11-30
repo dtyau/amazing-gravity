86c8b03b84fbb71a78628a59fb588e0e01b9e205
DANIEL AU <dantkau@gmail.com>
2014-11-30 04:55:36 -0800

    created ObstacleManager class

---
+++ app/src/main/java/com/studiau/amazinggravity/ObstacleManager.java
@@ -0,0 +1,21 @@
+package com.studiau.amazinggravity;
+
+import java.util.ArrayList;
+
+/**
+ * Author: Daniel Au.
+ */
+
+public class ObstacleManager {
+
+    public ObstacleManager() {
+
+        obstacles = new ArrayList<Obstacle>();
+
+        obstacles.add(new Obstacle());
+
+    }
+
+    private ArrayList<Obstacle> obstacles;
+
+}
